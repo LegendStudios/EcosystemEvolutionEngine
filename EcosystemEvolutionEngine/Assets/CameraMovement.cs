7142a324b8c127b92da0b1792694883197e7ced0
RudeSquared <63614420+RudeSquared@users.noreply.github.com>
2020-04-15 13:06:17 -0600

    Added Camera Controls
    I also got it moving. Needs some refinement bu it shoul work just fine. You can use either wasd or arrow keys.

---
+++ EcosystemEvolutionEngine/Assets/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float CameraSpeed= 18;
+
+    private Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * CameraSpeed * Time.deltaTime;
+
+        rig.MovePosition(transform.position + movement);
+    }
+}
